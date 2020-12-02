b3f4683977d5fde2efbe55df5a7778d9efa26aa7
Phan Đình Hoàng <hoangphan3565@gmail.com>
2020-12-02 15:49:37 +0700

    update

--- charitysystem/src/main/java/com/macia/charitysystem/api/PaypalController.java
+++ charitysystem/src/main/java/com/macia/charitysystem/api/PaypalController.java
@@ -1,8 +1,15 @@
 package com.macia.charitysystem.api;
 
 import com.macia.charitysystem.model.AppUser;
+import com.macia.charitysystem.model.DonateActivity;
+import com.macia.charitysystem.model.DonateDetails;
 import com.macia.charitysystem.model.Order;
+import com.macia.charitysystem.repository.DonateDetailsRepository;
+import com.macia.charitysystem.service.DonateActivityService;
+import com.macia.charitysystem.service.DonatorService;
 import com.macia.charitysystem.service.PaypalService;
+import com.macia.charitysystem.service.ProjectService;
+import com.macia.charitysystem.utility.MoneyUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +18,10 @@ import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 
-//@Controller
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/paypal")
@@ -20,18 +30,34 @@ public class PaypalController {
 	@Autowired
 	PaypalService service;
 
+	@Autowired
+	DonateActivityService donateActivityService;
+
+	@Autowired
+	DonatorService donatorService;
+
+	@Autowired
+	ProjectService projectService;
+
+	@Autowired
+	DonateDetailsRepository donateDetailsRepository;
+
+
 	@GetMapping()
 	public String home() {
 		return "paypal home";
 	}
 
-	@PostMapping("/pay")
-	public String payment(Order order) {
-		order.setPrice(100);
+	@Transactional
+	@PostMapping("/donatorid/{did}/projectid/{pid}/donate")
+	public String payment(@PathVariable(value = "did") Integer donator_id,
+						  @PathVariable(value = "pid") Integer project_id,
+						  @RequestBody Order order) {
+		double price = order.getPrice();
+		order.setPrice(MoneyUtility.VNDToUSD(order.getPrice()));
 		order.setCurrency("USD");
 		order.setMethod("PAYPAL");
-		order.setIntent("authorize");
-		order.setDescription("Tesst from Hoangphan");
+		order.setIntent("sale"); //sale-thanh toán ngay | authorize-bên đặt mất tiền nhưng bên bán chưa có tiền | order-tạo đơn đặt hàng, cả 2 bên đều chưa có tiền
 		try {
 			Payment payment = service.createPayment(
 					order.getPrice(),
@@ -40,36 +66,56 @@ public class PaypalController {
 					order.getIntent(),
 					order.getDescription(),
 					"http://localhost:8080/api/paypal/cancel",
-					"http://localhost:8080/api/paypal/success");
+					"http://localhost:8080/api/paypal/success/donatorid/"+donator_id+"/projectid/"+project_id+"/money/"+price);
 			for(Links link:payment.getLinks()) {
 				if(link.getRel().equals("approval_url")) {
 					return "redirect:"+link.getHref();
 				}
 			}
-			
 		} catch (PayPalRESTException e) {
 			e.printStackTrace();
 		}
 		return "redirect:/";
 	}
-	
-	 @GetMapping("/cancel")
-	 public String cancelPay() {
-	        return "cancel";
-	    }
 
-	 @GetMapping("/success")
-	 public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
-	     try {
-	         Payment payment = service.executePayment(paymentId, payerId);
-	         System.out.println(payment.toJSON());
-	         if (payment.getState().equals("approved")) {
-	             return "success";
-	         }
-	     } catch (PayPalRESTException e) {
-	      System.out.println(e.getMessage());
-	     }
-	     return "redirect:/";
-	 }
+	@GetMapping("/cancel")
+	public String cancelPay() { return "cancel";}
 
+	@Transactional
+	@GetMapping("/success/donatorid/{did}/projectid/{pid}/money/{money}")
+	public String successPay(
+			@PathVariable(value = "did") Integer donator_id,
+			@PathVariable(value = "pid") Integer project_id,
+			@PathVariable(value = "money") Double money,
+			@RequestParam("paymentId") String paymentId,
+			@RequestParam("PayerID") String payerId) {
+		try {
+			Payment payment = service.executePayment(paymentId, payerId);
+			System.out.println(payment.toJSON());
+			if (payment.getState().equals("approved")) {
+				DonateActivity donateActivity = donateActivityService.findDonateActivityByDonatorIdAndProjectID(donator_id,project_id);
+				if(donateActivity==null){
+					donateDetailsRepository.save(DonateDetails.builder()
+							.donateActivity(donateActivityService.save(DonateActivity.builder()
+									.donator(donatorService.findById(donator_id))
+									.project(projectService.findProjectById(project_id))
+									.build()))
+							.donateDate(LocalDateTime.now())
+							.money(money)
+							.build());
+				}
+				else{
+					donateDetailsRepository.save(DonateDetails.builder()
+							.donateActivity(donateActivity)
+							.donateDate(LocalDateTime.now())
+							.money(money)
+							.build());
+				}
+				return "Ủng hộ thành công!";
+			}
+		} catch (PayPalRESTException e) {
+			System.out.println(e.getMessage());
+		}
+		return "Chuyển tiền không thành công";
+	}
 }
