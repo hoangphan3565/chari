fc17bbd5ae66d7accb0ff921b06dd09c45de28b0
Phan Đình Hoàng <hoangphan3565@gmail.com>
2020-12-23 22:09:23 +0700

    bonus! create function in database when start server

--- charitysystem/src/main/java/com/macia/charitysystem/CharitysystemApplication.java
+++ charitysystem/src/main/java/com/macia/charitysystem/CharitysystemApplication.java
@@ -7,14 +7,55 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @SpringBootApplication
 public class CharitysystemApplication implements CommandLineRunner {
 
     @Autowired
     DonatorService donatorService;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws SQLException {
         SpringApplication.run(CharitysystemApplication.class, args);
+
+        //Connect to existing database
+        String url = "jdbc:postgresql://localhost:5432/charitysystem?user=postgres&password=123";
+        Connection conn = DriverManager.getConnection(url);
+        //Creating the Statement
+        Statement stmt = conn.createStatement();
+        //Query to create a function
+        String query = "CREATE OR REPLACE FUNCTION public.get_projects_dto(\n" +
+                "\t)\n" +
+                "    RETURNS TABLE(prj_id integer, project_code character varying, project_name character varying, brief_description character varying, description character varying, image_url character varying, video_url character varying, target_money integer, cur_money integer, num_of_donations integer, remaining_term integer, prt_id integer, project_type_name character varying, status text)\n" +
+                "    LANGUAGE 'plpgsql'\n" +
+                "\n" +
+                "    COST 100\n" +
+                "    VOLATILE\n" +
+                "    ROWS 1000\n" +
+                "\n" +
+                "AS $BODY$\n" +
+                "begin\n" +
+                "\treturn query\n" +
+                "\t\tselect \tpr.prj_id, pr.project_code, pr.project_name,pr.brief_description,\n" +
+                "\t\t\t\tpr.description,pr.image_url,pr.video_url,pr.target_money,\n" +
+                "\t\t\t\tsum(dd.money)::integer as cur_money,count(dd.dnd_id)::integer as num_of_donations,\n" +
+                "\t\t\t\t(pr.end_date-DATE(now())) as remaining_term,pt.prt_id,pt.project_type_name,\n" +
+                "\t\t\t\tcase\n" +
+                "\t\t\t\t\twhen pr.end_date-DATE(now()) > 0 and sum(dd.money)::integer < pr.target_money then 'activating'\n" +
+                "\t\t\t\t\twhen pr.end_date-DATE(now()) > 0 and sum(dd.money)::integer >= pr.target_money  then 'reached'\n" +
+                "\t\t\t\t\twhen pr.end_date-DATE(now()) <= 0 then 'overdue'\n" +
+                "\t\t\t\tend as status\n" +
+                "\t\tfrom project pr, donate_activity da, donate_details dd,project_type pt\n" +
+                "\t\twhere pr.prj_id=da.prj_id and da.dna_id=dd.dna_id and pr.prt_id=pt.prt_id\n" +
+                "\t\tgroup by pr.prj_id,pt.prt_id,pt.project_type_name,pr.start_date\n" +
+                "        order by pr.start_date desc ;\n" +
+                "end;\n" +
+                "$BODY$;";
+        //Executing the query
+        stmt.execute(query);
     }
 
     @Override
